cc87f107bf54a0d9c9775b0499388b6cd03d1bea
polishedsolid <polishedsolid@gmail.com>
2014-01-07 19:03:38 +0900

    First

---
+++ Assets/CubeController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.mass = 3;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		print(collision.gameObject.name);
+		if (collision.gameObject.name == "Ball")
+		{
+			Destroy(this, 1.0f);
+		}
+	}
+}
---
+++ Assets/RollControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollControl : MonoBehaviour {
+
+	public int SpotCount = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+
+	private int SpotCounter = 0;
+
+	void Update () {
+		if (++SpotCounter > SpotCount)
+		{
+			transform.Rotate(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+			SpotCounter = 0;
+		}
+	}
+}

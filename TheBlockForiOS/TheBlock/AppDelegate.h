cb69c86e758427a361b1358b04bcfbbd1b1245a0
polishedsolid <polishedsolid@gmail.com>
2014-01-09 01:50:11 +0900

    Home

---
+++ TheBlockForiOS/TheBlock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TheBlock
+//
+//  Created by PHATLAB on 2014/01/08.
+//  Copyright (c) 2014年 phatlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TheBlockForiOS/TheBlock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TheBlock
+//
+//  Created by PHATLAB on 2014/01/08.
+//  Copyright (c) 2014年 phatlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
